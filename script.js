553db887dc56fe2c7bc6659e6ef964d4fc7451ac
RmSources <125315214+RMSources@users.noreply.github.com>
2024-04-01 15:14:47 -0300

    feat: create home page

---
+++ script.js
@@ -0,0 +1,10 @@
+var linkMeuSite = document.getElementById("linkMeuSite");
+var linkBemVindo = document.getElementById("linkBemVindo");
+
+linkMeuSite.addEventListener("click", function() {
+    window.open("meusite.html", "_blank");
+});
+
+linkBemVindo.addEventListener("click", function() {
+    window.open("bemvindo.html", "_blank");
+});
